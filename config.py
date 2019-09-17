b284fda600edddef516202f1cbbe1e23955df60e
bugong <1761869346@qq.com>
2019-09-18 00:31:11 +0800

    chore: format code

--- config.py
+++ config.py
@@ -9,16 +9,19 @@ APP_MODE = os.getenv('APP_MODE')
 
 conf = configparser.ConfigParser()
 
+
 def init_conf():
-  if APP_MODE == 'test':
-    path = get_file_path('test.conf')
-  elif APP_MODE == 'prod':
-    path = get_file_path('prod.conf')
-  else:
-    path = get_file_path('dev.conf')
-  conf.read(path)
+    if APP_MODE == 'test':
+        path = get_file_path('test.conf')
+    elif APP_MODE == 'prod':
+        path = get_file_path('prod.conf')
+    else:
+        path = get_file_path('dev.conf')
+    conf.read(path)
+
 
 def get_file_path(filename):
-  return os.path.abspath(os.path.join(os.getcwd(), 'conf', filename))
+    return os.path.abspath(os.path.join(os.getcwd(), 'conf', filename))
+
 
 init_conf()
--- server.py
+++ server.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
 
-import os
-from flask import Flask, jsonify  
+from flask import Flask, jsonify
 from config import APP_MODE, conf
 from db import read_series
 
@@ -15,13 +14,16 @@ is_debug = True if APP_MODE == 'dev' or APP_MODE == None else False
 app = Flask(app_name)
 app.config['JSON_AS_ASCII'] = False
 
+
 @app.route('/')
 def home():
-  return 'welcome to tac!'
+    return 'welcome to tac!'
+
 
 @app.route('/get-series')
 def get_series():
-  list = read_series()
-  return jsonify(list)
+    list = read_series()
+    return jsonify(list)
 
-app.run(host = app_host, port = app_port, debug = is_debug)  
+
+app.run(host=app_host, port=app_port, debug=is_debug)
